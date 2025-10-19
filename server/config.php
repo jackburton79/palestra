f0cde517e660c6f5b7c8155c956f927dc6a9fcff
JackBurton79 <stefano.ceccherini@gmail.com>
2025-10-19 22:33:54 +0200

    Fix config.php

--- server/config.php
+++ server/config.php
@@ -1,6 +1,6 @@
 <?php
-    $dbUsername = "gym";
-    $dbPassword = "3jgr7!134j13f";
-    $dbHost = "localhost";
+    $dbUser = "gym";
+    $dbPass = "3jgr7!134j13f";
+    $dbHost = "database";
     $dbName = "gym";
 ?>
